c7fb4a777f1a3f847a41e5440482b36347f7cc50
ChauNgocMinh <chaungocminh202@gmail.com>
2025-09-22 23:46:11 +0700

    update UI

--- E-Learning/wwwroot/js/site.js
+++ E-Learning/wwwroot/js/site.js
@@ -1,1 +1,55 @@
-﻿
+﻿(function ($) {
+    "use strict";
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+
+    new WOW().init();
+
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').addClass('bg-white shadow-sm').css('top', '0px');
+        } else {
+            $('.sticky-top').removeClass('bg-white shadow-sm').css('top', '-150px');
+        }
+    });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+    $(".header-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        loop: true,
+        dots: true,
+        items: 1
+    });
+
+    $(".testimonial-carousel").owlCarousel({
+        items: 1,
+        autoplay: true,
+        smartSpeed: 1000,
+        animateIn: 'fadeIn',
+        animateOut: 'fadeOut',
+        dots: true,
+        loop: true,
+        nav: false
+    });
+
+})(jQuery);
+
