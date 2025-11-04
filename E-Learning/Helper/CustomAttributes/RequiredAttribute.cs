38ea2c152dbe3f85ffb153d00fad8a0fa3098f3e
ChauNgocMinh <chaungocminh202@gmail.com>
2025-11-04 23:36:21 +0700

    update

---
+++ E-Learning/Helper/CustomAttributes/RequiredAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace E_Learning.Helper.CustomAttributes;
+
+[AttributeUsage(AttributeTargets.Property)]
+public class RequiredAttribute : Attribute
+{
+    public string? ErrorMessage { get; }
+
+    public RequiredAttribute(string? errorMessage = null)
+    {
+        ErrorMessage = errorMessage ?? "Đây là trường bắt buộc";
+    }
+}
