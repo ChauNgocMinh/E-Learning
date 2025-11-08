7d728221590b7911868cb1c5b1da5a01f43bb98d
tutl260994-cpu <trancongchuc3@gmail.com>
2025-11-09 01:11:24 +0700

    Update Base

--- E-Learning/Controllers/SystemControllers/BaseController.cs
+++ E-Learning/Controllers/SystemControllers/BaseController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using E_Learning.Helper.CustomAttributes;
+using Microsoft.AspNetCore.Mvc;
 
 namespace E_Learning.Controllers.SystemControllers;
 
 [Route("/[controller]/[action]")]
+[CustomValidateModel]
 public class BaseController : Controller
 {
 }
